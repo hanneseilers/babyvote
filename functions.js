338bf5694108b338b079e52efa542bb010dce567
Hannes Eilers <hanneseilers@gmx.de>
2024-03-13 16:11:13 +0100

    Fixed bug

--- functions.js
+++ functions.js
@@ -1,10 +1,4 @@
 function vote(gender) {
-    // Check if the user has already voted
-    if (hasVoted()) {
-        alert("You have already voted.")
-        return; // Stops the function if the user has already voted
-    }
-
     fetch('vote.php', {
         method: 'POST',
         headers: {
